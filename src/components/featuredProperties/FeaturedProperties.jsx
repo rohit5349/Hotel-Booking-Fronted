a60c24dd4737d4f1db7b373ff5e19bb79e82454e
rohit_singh12 <119621966+rohit5349@users.noreply.github.com>
2025-03-02 15:11:12 +0530

    Update FeaturedProperties.jsx

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -3,7 +3,7 @@ import useFetch from "../../hooks/useFetch";
 
 const FeaturedProperties = () => {
 
-  const { data , loading } = useFetch("/hotels?featured=true&limit=4");
+  const { data = [] , loading } = useFetch("/hotels?featured=true&limit=4");
 
   return (
     <div className="fp">
